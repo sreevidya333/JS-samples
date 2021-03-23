b8dd01946ff33a40ac3bb5ab56667362baa81898
sreevidya333 <sreevidya.chilamakur@gmail.com>
2021-03-23 21:01:09 +0530

    sample-files

---
+++ day1-3_samples/Arrays_object.js
@@ -0,0 +1,62 @@
+// _____Arrays____
+// var data=[1,2,3,4]
+// console.log(data)
+
+// _____object_____
+// var information={
+//     name:"vidya",
+//     phone:"123456789",
+//     email:"vidya @ gmail.com"
+// }
+// // console.log(information)
+
+// var methods={
+//      name:"shannu",
+//      class:"2nd",
+//      section:"B",
+//      school:"Dps",
+//      Area:"whitefeild",
+//      City:"bengalore",
+//      phone:"123456789",
+//      email:"shannu @ gmail.com"
+// }
+// console.log(methods)
+
+// _______updation_____
+// methods.name="mani-medhamsh"
+// console.log(methods)
+
+// ______insertion_______
+// methods.name2="shannu"
+// console.log(methods)
+
+// ___nested object_____
+
+var information={
+    country:{
+        name:"india",
+         state:{
+             name:"karnataka",
+             city:{
+                 name:"bengalore",
+                area:{
+                  name:"whitefeild",
+                  adress:{
+                      name:"sreevidya"
+                  }
+                 }
+             }
+            }
+        
+        
+    }
+}
+
+console.log(information.country.state.city.area.adress)
+console.log(information.country.state.city)
+console.log(information.country.state)
+
+
+
+
+
---
+++ day1-3_samples/file.js
@@ -0,0 +1,31 @@
+// function fn(){
+//     var information = "valid";
+//     console.log (information)
+// }
+// fn()
+
+// function fn1(x,y){
+//     var a = x
+//     var b = y
+//     console.log (a)
+//     console.log (b)
+// }
+// fn1(2,3)
+// function fn(x,y){
+//     var a=x;
+//     var b=y;
+//     console.log (a+b)
+// }
+// fn("a2n","acadeny")
+function fn(x,y){
+    var a=x;
+    var b=y;
+    console.log (a+b)
+}
+fn(500,500)
+function fn(x,y){
+    var a=x;
+    var b=y;
+    console.log(a+b)
+}
+fn("500","500")
